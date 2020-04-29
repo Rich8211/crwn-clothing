0046c819a78ae1264790ef2b9145799d5211d806
Rich <Richlee821@gmail.com>
2020-04-29 02:59:14 -0400

    Updated app to redirect if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
-import { useDispatch} from 'react-redux';
+import React, { useEffect } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
-import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
 
 import Header from "./components/header/header";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 const App = () => {
-
   const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.user.currentUser);
 
   useEffect(() => {
     const unsubcribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          dispatch({type:'SET_CURRENT_USER', payload: {
-            id: snapShot.id,
-            ...snapShot.data()}})
+          dispatch({
+            type: "SET_CURRENT_USER",
+            payload: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
         });
       }
-      dispatch({type:'SET_CURRENT_USER', payload: userAuth})
+      dispatch({ type: "SET_CURRENT_USER", payload: userAuth });
     });
     return () => unsubcribeFromAuth();
   }, []);
@@ -35,10 +39,16 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUp} />
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+          }
+        />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
