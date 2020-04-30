37652e34d03e3868fce47ccbdd2cdb3d18bfff30
Rich <Richlee821@gmail.com>
2020-04-30 01:48:37 -0400

    Updated shop data.js with new data, updated cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import "./collection-item.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ item }) => {
+  const dispatch = useDispatch();
+  const { id, name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => dispatch(addItem(item))}>
+        Add to cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
 export default CollectionItem;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-
 import "./index.css";
 import App from "./App";
 
@@ -12,7 +11,7 @@ import store from "./redux/store";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <App />~
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
