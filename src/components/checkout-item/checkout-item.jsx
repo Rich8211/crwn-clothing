d7b16af636d6e45d63f4565a70536efd3965c3da
Rich <Richlee821@gmail.com>
2020-05-03 23:53:21 -0400

    added increase/decrease quantity functionality at checkout, also added remove item from checkout

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,17 +1,40 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/cart/cart.actions";
 
 import "./checkout-item.scss";
 
-const CheckoutItem = ({ cartItem: { name, quantity, price, imageUrl } }) => {
+const CheckoutItem = ({ cartItem }) => {
+  const { name, quantity, price, imageUrl } = cartItem;
+  const dispatch = useDispatch();
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => dispatch(removeItem(cartItem))}>
+          {" "}
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => dispatch(addItem(cartItem))}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div
+        className="remove-button"
+        onClick={() => dispatch(clearItemFromCart(cartItem))}
+      >
+        &#10005;
+      </div>
     </div>
   );
 };
